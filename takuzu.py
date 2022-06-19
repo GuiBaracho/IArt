4df8d0cd6b01b4678ec04f30a8823daf79ddbd19
Guilherme Baracho <guilherme.baracho@tecnico.ulisboa.pt>
2022-06-19 19:27:07 +0100

    works for the guideline examples and inputs one and 2 (for the others is too slow) - Gives all possibel actions

--- takuzu.py
+++ takuzu.py
@@ -33,40 +33,37 @@ class TakuzuState:
 
     # Outros metodos da classe
 
-    def bd(self):
-        return self.board
-
-
 class Board:
-    def __init__(self, n, board) -> None:
+    def __init__(self, n, board, free) -> None:
         """Representação interna de um tabuleiro de Takuzu."""
         self.s = n
         self.l = board
+        self.f = free
 
     def get_number(self, row: int, col: int) -> int:
         """Devolve o valor na respetiva posição do tabuleiro."""
-        return self.l[row - 1][col - 1]
+        return self.l[row][col]
 
     def adjacent_vertical_numbers(self, row: int, col: int):
         """Devolve os valores imediatamente abaixo e acima,
         respectivamente."""
         up = None
         down = None
-        if(row - 1 > 0):
-            up = self.l[row - 2][col - 1]
-        if(row < self.s):
-            down = self.l[row][col - 1]
-        return (up, down)
+        if(row > 0):
+            up = self.l[row - 1][col]
+        if(row < self.s - 1):
+            down = self.l[row + 1][col]
+        return (down, up)
 
     def adjacent_horizontal_numbers(self, row: int, col: int):
         """Devolve os valores imediatamente à esquerda e à direita,
         respectivamente."""
         left = None
         right = None
-        if(col - 1 > 0):
-            left = self.l[row - 1][col - 2]
-        if(col < self.s):
-            right = self.l[row - 1][col]
+        if(col > 0):
+            left = self.l[row][col - 1]
+        if(col < self.s - 1):
+            right = self.l[row][col + 1]
         return (left, right)
 
     @staticmethod
@@ -83,33 +80,45 @@ class Board:
         from sys import stdin
         input = stdin.read()
         input = input.split('\n')
+        free = 0
         n = int(input[0])
         input = input[1:(n + 1)]
-
+        
         for i in range(n):
             input[i] = input[i].split('\t')
             input[i] = [int(x) for x in input[i]]
+            for y in input[i]:
+                if y == 2:
+                    free += 1
 
-        return Board(n, input)
+        return Board(n, input, free)
 
     # Outros metodos da classe
-    def lyt(self):
-        return self.l
-
-    def size(self) -> int:
-        return self.s
-
     def set_number(self, row, col, val):
-        self.l[row - 1][col - 1] = val
+        self.l[row][col] = val
+        self.f -= 1
 
-    def get_col(self, n):
-        col = []
-        for row in self.l:
-            col.append(row(n - 1))
-        return col
+    def transpose(self):
+        n = self.s
+        board = []
+        for i in range(n):
+            col = []
+            for row in self.l:
+                col.append(row[i])
+            board.append(col)
+        return board
 
-    def get_row(self, n):
-        return self.l[n - 1]
+    def chk_values(self):
+        n = self.s
+        b = self.l
+        t = self.transpose()
+        for i in range(n):
+            if (chk_line(n, b[i]) == False) or (chk_line(n, t[i]) == False):
+                return False
+            for j in range(n-i): 
+                if ((b[j+i] == b[i]) or (t[j+i] == t[i])) and (j+i) != i:
+                    return False
+        return True
 
     def copy(self):
         board = []
@@ -117,14 +126,17 @@ class Board:
         for row in self.l:
             board.append(row[:])
 
-        return Board(self.s, board)
+        return Board(self.s, board, self.f)
 
     def toString (self):
         s = ''
         for row in self.l:
-            for val in row:
-                s += str(val) + '\t'
-            s += '\n'
+            for i in range(self.s):
+                s += str(row[i])
+                if i == self.s-1:
+                    s += '\n'
+                else:
+                    s += '\t'
         return s
     
     def __str__(self):
@@ -133,20 +145,27 @@ class Board:
 class Takuzu(Problem):
     def __init__(self, board: Board):
         """O construtor especifica o estado inicial."""
-        self = board
+        self.initial = TakuzuState(board)
 
     def actions(self, state: TakuzuState):
         """Retorna uma lista de ações que podem ser executadas a
         partir do estado passado como argumento."""
-        # TODO
-        pass
+        b = state.board
+        n = b.s
+        actions = []
+        for row in range(n):
+            for col in range(n):
+                if b.get_number(row, col) == 2:
+                    actions.append((row, col, 0))
+                    actions.append((row, col, 1))
+        return actions
 
     def result(self, state: TakuzuState, action):
         """Retorna o estado resultante de executar a 'action' sobre
         'state' passado como argumento. A ação a executar deve ser uma
         das presentes na lista obtida pela execução de
         self.actions(state)."""
-        board = state.bd().copy()
+        board = state.board.copy()
         board.set_number(action[0], action[1], action[2])
         return TakuzuState(board)
 
@@ -154,8 +173,7 @@ class Takuzu(Problem):
         """Retorna True se e só se o estado passado como argumento é
         um estado objetivo. Deve verificar se todas as posições do tabuleiro
         estão preenchidas com uma sequência de números adjacentes."""
-        # TODO
-        pass
+        return ((state.board.f == 0) and (state.board.chk_values()))
 
     def h(self, node: Node):
         """Função heuristica utilizada para a procura A*."""
@@ -165,22 +183,31 @@ class Takuzu(Problem):
     # TODO: outros metodos da classe
 
 
+def chk_line(n, line):
+    max = abs(-n//2)
+    count_0, count_1, seq_0, seq_1 = 0, 0, 0, 0
+    for x in line:
+        if x == 0:
+            count_0 += 1
+            seq_0 += 1
+            seq_1 = 0
+        elif x == 1:
+            count_1 += 1
+            seq_0 = 0
+            seq_1 += 1
+        if (count_0 > max) or (count_1 > max) or (seq_1 > 2) or (seq_0 > 2):
+                return False
+    return True
+
+
 if __name__ == "__main__":
 
     board = Board.parse_instance_from_stdin()
-    print("Initial:\n", board, sep="")
     # Criar uma instância de Takuzu:
     problem = Takuzu(board)
-    # Criar um estado com a configuração inicial:
-    initial_state = TakuzuState(board)
-    # Mostrar valor na posição (2, 2):
-    print(initial_state.board.get_number(2, 2))
-    # Realizar acção de inserir o número 1 na posição linha 2 e coluna 2
-    result_state = problem.result(initial_state, (2, 2, 1))
-    # Mostrar valor na posição (2, 2):
-    print(result_state.board)
-    print(result_state.board.get_number(2, 2))
-    
+    # Obter o nó solução usando a procura em profundidade:
+    goal_node = depth_first_tree_search(problem)
+    print(goal_node.state.board, end="")
 
     # TODO:
     # Ler o ficheiro do standard input,
